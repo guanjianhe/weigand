9dab3748013b21673ed78909d701a36be26fd128
zhengxin <43071004+guanjianhe@users.noreply.github.com>
2019-08-28 22:19:36 +0800

    Update wiegand.h

--- wiegand.h
+++ wiegand.h
@@ -5,8 +5,8 @@
 
 extern void WIEGAND_init(void);
 extern uint32_t WIEGAND_getCode(void);
-extern int WIEGAND_getWiegandType(void);
-extern int WIEGAND_getWiegandType(void);
+extern uint8_t WIEGAND_getWiegandType(void);
+extern int WIEGAND_available(void);
 extern void WIEGAND_ReadD0(void);
 extern void WIEGAND_ReadD1(void);
 
